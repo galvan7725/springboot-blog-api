8537f9be8515eeaa47396cce48d7fb05b3d36a8a
galvan9001 <galvan7725@gmail.com>
2023-02-14 19:10:21 -0600

    Create ResourceNotFoundException.java

---
+++ src/main/java/com/luisgalvan/blog/exception/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package com.luisgalvan.blog.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+    private String resourceName;
+    private String fieldName;
+    private String fieldValue;
+
+    public ResourceNotFoundException(String resourceName, String fieldName, String fieldValue) {
+        super(String.format(""));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+}
