3d70f21e7a17c1cd8245306412fbc17864a00054
galvan9001 <galvan7725@gmail.com>
2023-02-16 18:58:37 -0600

    Create PostController.java

---
+++ src/main/java/com/luisgalvan/blog/controller/PostController.java
@@ -0,0 +1,27 @@
+package com.luisgalvan.blog.controller;
+
+import com.luisgalvan.blog.dto.PostDto;
+import com.luisgalvan.blog.service.PostService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/posts")
+public class PostController {
+
+
+    private PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> createPost(@RequestBody PostDto postDto){
+        return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+}
