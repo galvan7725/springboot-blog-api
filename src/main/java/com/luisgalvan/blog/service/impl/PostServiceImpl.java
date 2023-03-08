b52d6a0c088d0552cfa3ff4b01a2d42483b52bda
galvan9001 <galvan7725@gmail.com>
2023-03-08 17:29:32 -0600

    New method to get all posts

--- src/main/java/com/luisgalvan/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/luisgalvan/blog/service/impl/PostServiceImpl.java
@@ -6,6 +6,9 @@ import com.luisgalvan.blog.repository.PostRepository;
 import com.luisgalvan.blog.service.PostService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PostServiceImpl implements PostService {
 
@@ -17,19 +20,35 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public PostDto createPost(PostDto postDto) {
-        //convert DTO to entity
+
+        Post post = mapToEntity(postDto);
+       Post newPost = postRepository.save(post);
+       //convert entity to DTO
+        PostDto postResponse = mapToDTO(newPost);
+        return postResponse;
+    }
+
+    @Override
+    public List<PostDto> getAllPosts() {
+        List<Post> posts = postRepository.findAll();
+        return posts.stream().map(post -> mapToDTO(post)).collect(Collectors.toList());
+    }
+
+    private PostDto mapToDTO(Post post){
+        PostDto postDto = new PostDto();
+        postDto.setId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setContent(post.getContent());
+        postDto.setDescription(post.getDescription());
+
+        return postDto;
+    }
+
+    private Post mapToEntity(PostDto postDto){
         Post post = new Post();
         post.setTitle(postDto.getTitle());
         post.setDescription(postDto.getDescription());
         post.setContent(postDto.getContent());
-
-       Post newPost = postRepository.save(post);
-       //convert entity to DTO
-        PostDto postResponse = new PostDto();
-        postResponse.setId(newPost.getId());
-        postResponse.setTitle(newPost.getTitle());
-        postResponse.setDescription(newPost.getDescription());
-        postResponse.setContent(newPost.getContent());
-        return postResponse;
+        return post;
     }
 }
