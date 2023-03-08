1582cd5574d5553333494c92b9f136e7cd967e01
galvan9001 <galvan7725@gmail.com>
2023-03-08 17:29:09 -0600

    New method to getAll posts

--- src/main/java/com/luisgalvan/blog/service/PostService.java
+++ src/main/java/com/luisgalvan/blog/service/PostService.java
@@ -2,6 +2,10 @@ package com.luisgalvan.blog.service;
 
 import com.luisgalvan.blog.dto.PostDto;
 
+import java.util.List;
+
 public interface PostService {
     PostDto createPost(PostDto postDto);
+
+    List<PostDto> getAllPosts();
 }
